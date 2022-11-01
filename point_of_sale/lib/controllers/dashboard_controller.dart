704f831bc101d3427f629dc28ed91d747bd998d0
yangajila <griangajila2@gmail.com>
2022-11-01 23:38:37 +0800

    removing some files

--- point_of_sale/lib/controllers/dashboard_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class DashBoardController extends ChangeNotifier
-{
-  
-}
--- point_of_sale/lib/models/printermodel.dart
+++
@@ -1,7 +0,0 @@
-class PrinterModel {
-  String? name;
-  String? macAddress;
-  bool? isconnected;
-
-  PrinterModel({this.name, this.macAddress, this.isconnected});
-}
